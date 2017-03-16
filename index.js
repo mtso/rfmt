f770cd6a0f55129a041eefde98fae9e8d7521321
mtso <mtso@tuta.io>
2017-03-16 13:32:17 -0700

    Complete fmt entry file to format and write over using prettier

---
+++ index.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+const prettier = require('prettier');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * prettier.js options
+ */
+const options = {
+  printWidth: 72,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+
+  // Here for reference.
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babylon'
+};
+
+/* Begin execution here */
+
+var files = fs.readdirSync(__dirname);
+
+files = files.filter(function(filename) {
+  return filename.endsWith('.js') && !filename.startsWith('.');
+});
+
+files.forEach(function(file) {
+  format(file, options, 'utf8');
+});
+
+/* Formatter */
+
+function format(filename, options, encoding) {
+  var source = fs.readFileSync(filename, encoding);
+  var formatted = prettier.format(source, options);
+  if (source !== formatted) {
+    fs.writeFileSync(filename, formatted, encoding);
+    console.log(filename);
+  }
+}
